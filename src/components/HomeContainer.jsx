a51ce1a3d584feedd03a6bd3229033035d16ae0b
krish-98 <muralikrishnanm98@gmail.com>
2022-06-08 21:08:59 +0530

    home container right side of the section (part-1)

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import Delivery from "../img/delivery.png"
+import HeroBg from "../img/heroBg.png"
 
 const HomeContainer = () => {
   return (
@@ -37,6 +38,16 @@ const HomeContainer = () => {
         <button className="bg-gradient-to-br from-orange-400 to-orange-400 w-full md:w-auto px-4 py-2 rounded-lg transition-all ease-in-out duration-100">
           Order Now
         </button>
+      </div>
+
+      {/* right side of the section */}
+      <div className="py-2 flex-1 flex items-center">
+        <img
+          src={HeroBg}
+          className="ml-auto h-420 w-full lg:w-auto lg:h-650"
+          alt="hero-bg"
+        />
+        <div className="w-full h-full absolute flex items-center justify-center"></div>
       </div>
     </section>
   )
