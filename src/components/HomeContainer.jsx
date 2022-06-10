57dd1d4e3c2dafcc65aac0ee666713cbdd295cc6
krish-98 <muralikrishnanm98@gmail.com>
2022-06-10 11:37:50 +0530

    home container right side of the section (part-2)

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import Delivery from "../img/delivery.png"
 import HeroBg from "../img/heroBg.png"
 
+import { heroData } from "../utils/data"
+
 const HomeContainer = () => {
   return (
     <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
@@ -41,13 +43,38 @@ const HomeContainer = () => {
       </div>
 
       {/* right side of the section */}
-      <div className="py-2 flex-1 flex items-center">
+      <div className="py-2 flex-1 flex items-center relative">
         <img
           src={HeroBg}
           className="ml-auto h-420 w-full lg:w-auto lg:h-650"
           alt="hero-bg"
         />
-        <div className="w-full h-full absolute flex items-center justify-center"></div>
+
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center gap-4 flex-wrap py-4 lg:px-32">
+          {heroData &&
+            heroData.map((data) => (
+              <div
+                key={data.id}
+                className="lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+              >
+                <img
+                  src={data.imageSrc}
+                  className="w-20 lg:w-40 -mt-10 lg:-mt-20"
+                  alt="I1"
+                />
+                <p className="text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4">
+                  {data.name}
+                </p>
+                <p className="text=[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3">
+                  {data.desc}
+                </p>
+                <p className="text-sm font-semibold text-headingColor">
+                  <span className="text-xs text-red-600">$</span>
+                  {data.price}
+                </p>
+              </div>
+            ))}
+        </div>
       </div>
     </section>
   )
---
+++ src/utils/data.js
@@ -0,0 +1,35 @@
+import I1 from "../img/i1.png"
+import F1 from "../img/f1.png"
+import C3 from "../img/c3.png"
+import Fi1 from "../img/fi1.png"
+
+export const heroData = [
+  {
+    id: 1,
+    name: "Icecream",
+    desc: "Chocolate & vanilla",
+    price: "5.25",
+    imageSrc: I1,
+  },
+  {
+    id: 2,
+    name: "Strawberries",
+    desc: "Fresh Strawberries",
+    price: "10.25",
+    imageSrc: F1,
+  },
+  {
+    id: 3,
+    name: "Chicken Kebab",
+    desc: "Mixed Kebab Plate",
+    price: "18.95",
+    imageSrc: C3,
+  },
+  {
+    id: 4,
+    name: "Fish",
+    desc: "Mixed Kebab Fish",
+    price: "13.75",
+    imageSrc: Fi1,
+  },
+]
